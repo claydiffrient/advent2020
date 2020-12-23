193d1d53d7aac71513546c77afa7e98a135255a1
Clay Diffrient <clay.diffrient@gmail.com>
2020-12-23 14:13:56 -0700

    Fix coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
-  collectCoverageFrom: ["packages/**/*.{ts}", "!**/node_modules/**"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/run*", "!**/node_modules/**"],
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
   transformIgnorePatterns: ["^.+\\.js$"],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+/* istanbul ignore file */
 import { runOne } from "./one";
+import { runTwo } from "./two";
+import { runThree } from "./three";
 
 export function runAll() {
   runOne();
+  runTwo();
+  runThree();
 }
